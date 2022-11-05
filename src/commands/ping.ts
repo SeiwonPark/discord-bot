da3f25a673e1313df280a8d5e9a9a19705b54eaa
SeiwonPark <psw7347@gmail.com>
2022-11-05 20:51:12 +0900

    chore: added function comments

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,15 +3,23 @@ import { SlashCommand } from "src/interfaces";
 
 export const Ping: SlashCommand = {
   name: "ping",
-  description: "Returns a 'pong' ",
+  description: "Returns a 'pong'",
+  /**
+   * CommandInteraction types
+   *
+   * 1: Ping,
+   * 2: ApplicationCommand,
+   * 3: MessageComponent,
+   * 4: ApplicationCommandAutocomplete,
+   * 5: ModalSubmit
+   */
   type: 1,
   run: async (client: Client, interaction: CommandInteraction) => {
     const content = "᭼ ᭼ ᭼ ᭼ 💥🏓 pong!";
-
     await interaction.followUp({
       ephemeral: true,
       content,
     });
   },
-  cooldown: 10,
+  cooldown: 5, // expiration time(unit: seconds)
 };
--- src/handler/slash.ts
+++ src/handler/slash.ts
@@ -9,11 +9,18 @@ export default (client: Client): void => {
   });
 };
 
+/**
+ * Wrapper and Runner for Slash Command
+ * @param client Discord client instance
+ * @param interaction The message that your application receives when a user uses an application command or a message component
+ */
 const handleSlashCommand = async (
   client: Client,
   interaction: CommandInteraction
 ): Promise<void> => {
-  const slashCommand = Commands.find((c) => c.name === interaction.commandName);
+  const slashCommand = Commands.find(
+    (cmd) => cmd.name === interaction.commandName
+  );
   if (!slashCommand) {
     interaction.followUp({ content: "An error has occurred" });
     return;
